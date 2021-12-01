b61e40c060c078a4deeb0270e67808393028ce98
Pierce Trey <ptrey@figure.com>
2021-12-01 10:26:38 -0700

    add vscod config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,14 @@
 plugins {
     kotlin("jvm") version "1.6.0"
+    application
 }
 
 repositories {
     mavenCentral()
+}
+
+application {
+    mainClassName = if (project.hasProperty("mainClass")) project.properties["mainClass"]!! as String else "NULL"
 }
 
 tasks {
