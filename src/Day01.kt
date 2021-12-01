01feb95ca66f368cf2f8ea035a3ca8437c7b8512
Pierce Trey <ptrey@figure.com>
2021-12-01 09:34:27 -0700

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,28 @@
 fun main() {
+    data class DepthTracker(val previous: Int, val numIncreased: Int)
+    fun List<Int>.getDepthIncreases() = fold(DepthTracker(first(), 0)) { acc, curr ->
+        DepthTracker(curr, acc.numIncreased + if (curr > acc.previous) 1 else 0)
+    }.numIncreased
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val depths = input.map { it.toInt() }
+        return depths.getDepthIncreases()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val depths = input.map { it.toInt() }
+        return depths.windowed(3, 1, false) {
+            it.sum()
+        }.getDepthIncreases()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
 
     val input = readInput("Day01")
     println(part1(input))
+
+    check(part2(testInput) == 5)
     println(part2(input))
 }
